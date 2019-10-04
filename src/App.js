66e77df28ea0063c2b51ae6a05e3918560960a2e
Jared Rodriguez <jaredarodriguez@gmail.com>
2019-10-04 14:31:10 -0500

    style lab complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import GameBoard from './components/GameBoard/GameBoard';
+import ColorPicker from './components/ColorPicker/ColorPicker';
+import GameTimer from './components/GameTimer/GameTimer';
+import NewGameButton from './components/NewGameButton/NewGameButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const colors = ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD'];
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selColorIdx: 0,
+      guesses: [this.getNewGuess(), this.getNewGuess(), this.getNewGuess(), this.getNewGuess()],
+      code: this.genCode()
+    };
+  }
+
+  getNewGuess() {
+    return {
+      code: [],
+      score: {
+        perfect: 0,
+        almost: 0
+      }
+    };
+  }
+
+  genCode() {
+    return new Array(4).fill().map(dummy => Math.floor(Math.random() * 4));
+  }
+
+  getWinTries() {
+    let lastGuess = this.state.guesses.length - 1;
+    return this.state.guesses[lastGuess].score.perfect === 4 ? lastGuess + 1 : 0;
+  }
+
+  render() {
+    let winTries = this.getWinTries();
+    return (
+      <div className="App">
+        <header className='App-header-footer'>R E A C T &nbsp;&nbsp;&nbsp;  M A S T E R M I N D</header>
+        <div className="flex-h align-flex-end">
+          <GameBoard
+            colors={colors}
+            guesses={this.state.guesses}
+          />
+          <div className='App-controls'>
+            <ColorPicker
+              colors={colors}
+              selColorIdx={this.state.selColorIdx}
+            />
+            <GameTimer />
+            <NewGameButton />
+          </div>
+        </div>
+        <footer className='App-header-footer'>
+          {(winTries ? `You Won in ${winTries} Guesses!` : 'Good Luck!')}
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
